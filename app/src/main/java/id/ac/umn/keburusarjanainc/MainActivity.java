6f545541f1a955b76e67502c64a7564fbe440886
ImamW <imam.wiratama@gmail.com>
2019-04-10 19:51:16 +0700

    webView udh bisa

--- app/src/main/java/id/ac/umn/keburusarjanainc/MainActivity.java
+++ app/src/main/java/id/ac/umn/keburusarjanainc/MainActivity.java
@@ -3,17 +3,25 @@ package id.ac.umn.keburusarjanainc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private WebView webView;
+    private WebView myWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toolbar toolbarTop = (Toolbar) findViewById(R.id.nav_view);
-        TextView mTitle = (TextView) toolbarTop.findViewById(R.id.content_frame);
         setContentView(R.layout.activity_main);
+//        Toolbar toolbarTop = (Toolbar) findViewById(R.id.nav_view);
+//        TextView mTitle = (TextView) toolbarTop.findViewById(R.id.content_frame);
+        myWebView = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://www.ultimagz.com");
+
     }
 }
