e6cbb484f6367a1dadcebeddd824721220a2dc5e
nathan <berserk.nate@gmail.com>
2019-04-25 01:06:33 +0700

    Building Intended Home Activity Next

--- app/src/main/java/id/ac/umn/keburusarjanainc/MainActivity_Build.java
+++ app/src/main/java/id/ac/umn/keburusarjanainc/MainActivity_Build.java
@@ -1,13 +1,23 @@
 package id.ac.umn.keburusarjanainc;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity_Build extends AppCompatActivity {
+    private static ViewPager viewPager;
+    private static TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__build);
+
+        tabLayout = findViewById(R.id.toolbar);
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
