1ea1d1ad8e45a980b2c7ada677f4becc504b62e2
nathan <berserk.nate@gmail.com>
2019-04-25 01:05:54 +0700

    Building Intended Home Activity

---
+++ app/src/main/java/id/ac/umn/keburusarjanainc/MainActivity_Build.java
@@ -0,0 +1,13 @@
+package id.ac.umn.keburusarjanainc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_Build extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__build);
+    }
+}
