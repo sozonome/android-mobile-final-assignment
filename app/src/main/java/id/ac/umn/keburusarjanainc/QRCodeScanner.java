3ba3f9f8a9bba8b7e20ec9a8beb4740cfa4ae7eb
ImamW <imam.wiratama@gmail.com>
2019-04-25 00:02:14 +0700

    upadte terbaru fixed timeout & scanner

--- app/src/main/java/id/ac/umn/keburusarjanainc/QRCodeScanner.java
+++ app/src/main/java/id/ac/umn/keburusarjanainc/QRCodeScanner.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.SparseArray;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.google.android.gms.samples.vision.barcodereader.BarcodeCapture;
 import com.google.android.gms.samples.vision.barcodereader.BarcodeGraphic;
@@ -14,7 +17,7 @@ import java.util.List;
 import xyz.belvi.mobilevisionbarcodescanner.BarcodeRetriever;
 
 public class QRCodeScanner extends AppCompatActivity implements BarcodeRetriever{
-
+    private WebView myWebView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,10 +42,16 @@ public class QRCodeScanner extends AppCompatActivity implements BarcodeRetriever
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                AlertDialog.Builder builder = new AlertDialog.Builder(QRCodeScanner.this)
-                        .setTitle("Code Retrieved")
-                        .setMessage(barcode.displayValue);
-                builder.show();
+//                AlertDialog.Builder builder = new AlertDialog.Builder(QRCodeScanner.this)
+//                        .setTitle("Code Retrieved")
+//                        .setMessage(barcode.displayValue);
+//                builder.show();
+                setContentView(R.layout.activity_result_scan);
+
+                myWebView2 = (WebView)findViewById(R.id.webView2);
+                WebSettings webSettings = myWebView2.getSettings();
+                myWebView2.setWebViewClient(new WebViewClient());
+                myWebView2.loadUrl(barcode.displayValue);
             }
         });
     }
