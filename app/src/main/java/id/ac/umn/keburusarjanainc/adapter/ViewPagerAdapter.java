d1c23b789d2e67c29a47f4a281b53fe5b2be9307
nathan <berserk.nate@gmail.com>
2019-05-01 19:39:24 +0700

    Backup
    Masih Error

--- app/src/main/java/id/ac/umn/keburusarjanainc/adapter/ViewPagerAdapter.java
+++ app/src/main/java/id/ac/umn/keburusarjanainc/adapter/ViewPagerAdapter.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.util.Log;
+
 import id.ac.umn.keburusarjanainc.fragment.ListFragment;
 
 public class ViewPagerAdapter extends FragmentPagerAdapter {
@@ -15,24 +17,24 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-//        switch (position){
-//            case 0:
-//                ListFragment listFragment = new ListFragment();
-//                Log.d("ViewPagerAdapter", "Isi : " + listFragment);
-//                return listFragment;
-//            case 1:
-//                ListFragment2 listFragment2 = new ListFragment2();
-//                Log.d("ViewPagerAdapter", "Isi : " + listFragment2);
-//                return listFragment2;
-//        }
-//        return null;
+        switch (position){
+            case 0:
+                ListFragment listFragment = new ListFragment();
+                Log.d("ViewPagerAdapter", "Isi : " + listFragment);
+                return listFragment;
+            case 1:
+                ListFragment listFragment2 = new ListFragment("?categories?name=berita-kampus" );
+                Log.d("ViewPagerAdapter", "Isi : " + listFragment2);
+                return listFragment2;
+        }
+        return null;
 
-        ListFragment listFragment= new ListFragment();
-        position = position+1;
-        Bundle bundle = new Bundle();
-        bundle.putString("message", "Fragment :"+position);
-        listFragment.setArguments(bundle);
-        return listFragment;
+//        ListFragment listFragment= new ListFragment();
+//        position = position+1;
+//        Bundle bundle = new Bundle();
+//        bundle.putString("message", "Fragment :"+position);
+//        listFragment.setArguments(bundle);
+//        return listFragment;
     }
 
     @Override
@@ -44,7 +46,14 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
-        position = position +1;
-        return "Fragment " + position;
+        switch(position){
+            case 0:
+                String title1 = "Recent";
+                return title1;
+            case 1:
+                String title2 = "Berita Kampus";
+                return title2;
+        }
+        return null;
     }
 }
--- app/src/main/java/id/ac/umn/keburusarjanainc/fragment/ListFragment.java
+++ app/src/main/java/id/ac/umn/keburusarjanainc/fragment/ListFragment.java
@@ -1,23 +1,56 @@
 package id.ac.umn.keburusarjanainc.fragment;
 
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import id.ac.umn.keburusarjanainc.FetchFromJSON;
 import id.ac.umn.keburusarjanainc.R;
+import id.ac.umn.keburusarjanainc.adapter.ArticlesAdapter;
+import id.ac.umn.keburusarjanainc.model.ArticlesList;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ListFragment extends Fragment {
-    private TextView textView;
+//    private TextView textView;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private List<ArticlesList> articlesLists;
+
+    private static final String URL_DATA = "http://ultimagz.com/wp-json/wp/v2/posts";
+    private String URL_PARAM = "";
+//    private String title = "";
 
     public ListFragment() {
         // Required empty public constructor
+    }
+
+    public ListFragment(String URL_PARAM){
+        this.URL_PARAM = URL_PARAM;
     }
 
 
@@ -26,12 +59,69 @@ public class ListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_list, container, false);
-        textView = view.findViewById(R.id.text_display);
-        textView.setText(getArguments().getString("message"));
 
+        recyclerView = view.findViewById(R.id.newfeed_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
+        articlesLists = new ArrayList<>();
+        loadUrlData();
 
         return view;
     }
 
+    private void loadUrlData(){
+        final ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("Loading . . .");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,
+                URL_DATA + URL_PARAM, new Response.Listener<String>() {
+
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+                try {
+                    JSONArray array = new JSONArray(response);
+                    for(int i=0; i<array.length(); i++){
+                        JSONObject jo = array.getJSONObject(i);
+                        JSONObject jo_title = jo.getJSONObject("title");
+                        JSONObject jo_image = jo.getJSONObject("better_featured_image");
+                        JSONObject jo_content = jo.getJSONObject("content");
+                        ArticlesList articles = new ArticlesList(jo_title.getString("rendered"), jo_image.getString("source_url"), jo_content.getString("rendered"), jo.getString("date"));
+                        articlesLists.add(articles);
+                    }
+
+                    adapter = new ArticlesAdapter(articlesLists, getContext());
+                    recyclerView.setAdapter(adapter);
+                } catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getContext(), "Error" + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        stringRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 50000;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 50000;
+            }
+
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+        requestQueue.add(stringRequest);
+    }
 }
